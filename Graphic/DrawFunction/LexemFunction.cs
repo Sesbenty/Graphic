d3e37dd77cfd84405e84dacea29a4cf917d8444d
Sesbenty <Sesbenty@users.noreply.github.com>
2020-10-16 09:32:31 +0300

    Update LexemFunction

---
+++ Graphic/DrawFunction/LexemFunction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphic.DrawFunction
+{
+    class LexemFunction
+    {
+        static int current;
+        static string func;
+        public static void Function(string func)
+        {
+
+        }
+
+        public static void NextLexem()
+        {
+            switch (func[current])
+            {
+                case '+':
+                    break;
+                case '-':
+                    break;
+                case 'x':
+                    break;
+                case '*':
+                    break;
+                case '/':
+                    break;
+                case '0' - '9':
+                    break;
+            }
+        }
+
+        enum Lexem
+        {
+            number,
+            parametrX,
+            plus,
+            minus,
+            mult,
+            div
+        }
+    }
+}
